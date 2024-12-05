f522d8038cb4080b0d69b014502e0ac15f6dc782
Angeloredo1 <loredomarti@hotmail.com>
2024-12-05 03:53:43 -0600

    boss terminado

--- Cat-Attack/Assets/Scripts/BossBullet.cs
+++ Cat-Attack/Assets/Scripts/BossBullet.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BossBullet : MonoBehaviour
 {
     [SerializeField] private int danio = 25; 
+    private float minX = -0.11f, maxX= 8.4f;
+    private float minY = -4.6f, maxY = 0.01f;
 
     // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject,3);
+    }
+
+    void Update()
+    {
+        
+        
+        if(transform.position.y > maxY || transform.position.y < minY){
+            Destroy(gameObject);
+        }
+        if(transform.position.x > maxX || transform.position.x < minX){
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other){
